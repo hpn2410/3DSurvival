9ec04b85284632dc8df5e6fdb528d8c1fc72f7d2
pnu <94816672+hpn2410@users.noreply.github.com>
2025-05-08 01:22:41 +0700

    Loading_Scene (done)

--- 3DSurvivalGame/Assets/Scripts/GlobalState/MainMenu.cs
+++ 3DSurvivalGame/Assets/Scripts/GlobalState/MainMenu.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject guideMenu;
     public GameObject mainMenu_bg;
+    public GameObject loadingMenu;
+    public Slider loadingSlider;
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
-        Debug.LogWarning("Load Scene");
+        mainMenu_bg.SetActive(false);
+        loadingMenu.SetActive(true);
+        StartCoroutine(LoadSceneAsync(1));
+    }
+
+    IEnumerator LoadSceneAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        operation.allowSceneActivation = false;
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            loadingSlider.value = progress;
+
+            if (operation.progress >= 0.9f)
+            {
+                yield return new WaitForSeconds(0.5f);
+                operation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
     }
 
     public void Guide()
